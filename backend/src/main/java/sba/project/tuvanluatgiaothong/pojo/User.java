20a2a1fd88911ec7fa939d4c6a66a1ee0c3d398f
Manh Khoi <khoipmse182554@fpt.edu.vn>
2025-05-30 09:13:40 +0700

    Add User Management

--- backend/src/main/java/sba/project/tuvanluatgiaothong/pojo/User.java
+++ backend/src/main/java/sba/project/tuvanluatgiaothong/pojo/User.java
@@ -1,5 +1,58 @@
 package sba.project.tuvanluatgiaothong.pojo;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // @Column(name = "username", unique = true, length = 60)
+    // private String username;
+
+    @Column(name = "email", nullable = false, unique = true, length = 320)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @Column(name = "fullname", nullable = false, length = 100)
+    private String fullname;
+
+    @Column(name = "avatar_url", length = 1024)
+    private String avatarUrl;
+
+    @Column(name = "birthday")
+    private Date birthday;
+
+    @Column(name = "is_enable", nullable = false)
+    private boolean isEnable;
+
+    public enum Role {
+        USER, ADMIN
+    }
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp createdAt;
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
 }
